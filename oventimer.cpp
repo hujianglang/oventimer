5adee80220d4cc5d564a28556685dafc95d13361
hujianglang <hujianglang@126.com>
2017-01-11 22:57:41 +0800

    仍然

---
+++ oventimer.cpp
@@ -0,0 +1,27 @@
+#include "oventimer.h"
+
+OvenTimer::OvenTimer(QWidget *parent) :
+    QWidget(parent)
+{
+}
+
+void OvenTimer::setDuration(int secs)
+{
+}
+
+int OvenTimer::duration() const
+{
+}
+
+void OvenTimer::draw(QPainter *painter)
+{
+}
+
+
+void OvenTimer::paintEvent(QPaintEvent *event)
+{
+}
+
+void OvenTimer::mousePressEvent(QMouseEvent *event)
+{
+}
---
+++ oventimer.h
@@ -0,0 +1,18 @@
+#ifndef OVENTIMER_H
+#define OVENTIMER_H
+
+#include <QWidget>
+
+class OvenTimer : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OvenTimer(QWidget *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // OVENTIMER_H
